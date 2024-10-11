7ff73d3ce13542346433f11856515eb206edea21
Amidatelion <63753686+amidatelion@users.noreply.github.com>
2024-10-11 18:30:14 -0400

    checkpoint

--- factionRenderer.py
+++ factionRenderer.py
@@ -1,95 +1,84 @@
 from jinja2 import Environment, FileSystemLoader
 
 
-weapons = [
-    {"item": "[[Weapons#Cluster_Shot|Silver Bullet Gauss]]</br>"},
-    {"item": "[[Weapons#Hyper_Laser|Hyper Laser]]</br>"},
-    {"item": "[[Weapons#Weapons#X-Pulse_Lasers|Large X-Pulse Laser]]</br>"},
-    {"item": "[[Weapons#Weapons#X-Pulse_Lasers|Medium X-Pulse Laser]]</br>"},
-    {"item": "[[Weapons#Weapons#X-Pulse_Lasers|Small X-Pulse Laser]]</br>"},
-    {"item": "[[Weapons#MMLs|MML3]]</br>"},
-    {"item": "[[Weapons#MMLs|MML5]]</br>"},
-    {"item": "[[Weapons#MMLs|MML7]]</br>"},
-    {"item": "[[Weapons#MMLs|MML9]]</br>"},
-    {"item": "[[Weapons#PPCs|Snub-nosed PPC]]</br>"},
-    {"item": "[[mmunition#Gauss|Cluster Bomb]]</br>"},
-    {"item": "[[Weapons#Bomb Bays|HiEx Bomb]]</br>"},
-    {"item": "[[Weapons#Bomb Bays|Inferno Bomb]]</br>"},
-    {"item": "[[Weapons#Mech Taser|Mech Taser]]</br>"},
-    {"item": "[[Weapons#Rotary Autocannons|Rotary AC/2]]</br>"},
-    {"item": "[[Weapons#Rotary Autocannons|Rotary AC/5]]</br>"},
-    {"item": "[[Weapons#Ultra Autocannons|UAC/2]]</br>"},
-    {"item": "[[Weapons#Ultra Autocannons|UAC/5]]</br>"},
-    {"item": "[[Weapons#Ultra Autocannons|UAC/10]]</br>"},
-    {"item": "[[Weapons#Ultra Autocannons|UAC/20]]</br>"},
-]
-ammunitions = [
-    {"item": "[[Ammunition#|Ammo SB Gauss]]</br>"},
-    {"item": "[[Ammunition#|Ammo LRM]]</br>"},
-    {"item": "[[Ammunition#|Ammo SRM]]</br>"},
-    {"item": "[[Ammunition#|Ammo Artemis IV LRM]]</br>"},
-    {"item": "[[Ammunition#|Ammo Artemis IV SRM]]</br>"},
-]
-gears = [
-    {"item": "[[Weapon_Attachments#Artemis IV FCS|Artemis IV FCS]]</br>"},
-    {"item": "[[CASE#CASE_II|CASE II]]</br>"},
-    {"item": "[[ECM#Viral_Jammer|Viral Jammer]]</br>"},
-    {"item": "[[Cooling#Coolant_Pod|Coolant Pod]]</br>"},
-    {"item": "[[Cooling#Radical Heat Sink|Radical Heat Sink]]</br>"},
-    {"item": "[[Cooling#Heat_Sink_Kit_.28DHS.29|DHS Kit]]</br>"},
-    {"item": "{{Highlight|[[Heat_Sink_.28D.29|Heat Sink (D)]]|LightBlue}}</br>"},
-    {"item": "[[Upper_Arm_Actuators#Upper Arm Impede + +|Upper Arm Impede + +]]</br>"}
-]
-mechs = [
-    {"item": "[[Argus#AGS-4D|Argus AGS-4D]]</br>"},
-    {"item": "[[Battleaxe#BKX-8K|Battleaxe BKX-8K]]</br>"},
-    {"item": "[[Bushwacker#BSW-X1|Bushwacker BSW-X1]]</br>"},
-    {"item": "[[Chimera#CMA-1S|Chimera CMA-1S]]</br>"},
-    {"item": "[[Dervish#DV-7D|Dervish DV-7D]]</br>"},
-    {"item": "[[Enfield#END-6Q|Enfield END-6Q]]</br>"},
-    {"item": "[[Enforcer II#ENF-5D|Enforcer II ENF-5D]]</br>"},
-    {"item": "[[Fireball#ALM-8D|Fireball ALM-8D]]</br>"},
-    {"item": "[[JagerMech II#JM7-F|JagerMech II JM7-F]]</br>"},
-    {"item": "[[Osiris#OSR-3D|Osiris OSR-3D]]</br>"},
-    {"item": "[[Penetrator#PTR-4D|Penetrator PTR-4D]]</br>"},
-    {"item": "[[Salamander#PPR-5S|Salamander PPR-5S]]</br>"},
-    {"item": "[[Griffin_(60T)#GRF-2N-X|Super Griffin GRF-2N-X]]</br>"},
-    {"item": "[[Super Wasp#WSP-2A-X|Super Wasp WSP-2A-X]]</br>"},
-    {"item": "[[Swordsman#SWD-3|Swordsman SWD-3]]</br>"},
-    {"item": "[[Talon#TLN-5W|Talon TLN-5W]]</br>"},
-    {"item": "[[Templar#TLR1-O|Templar TLR1-O]]</br>"},
-    {"item": "[[Titan#TI-1A|Titan TI-1A]]</br>"},
-    {"item": "[[Valkyrie II#VLK-II-2A|Valkyrie II VLK-II-2A]]</br>"},
-    {"item": "[[Valkyrie#VLK-QD|Valkyrie VLK-QD]]</br>"},
-    {"item": "[[Wasp LAM#WSP-103|Wasp LAM WSP-103]]</br>"}
-]
-mechparts = [
-    {"item": "[[JagerMech II#JM7-D|JagerMech II JM7-D]]</br>"},
-    {"item": "[[Valkyrie II#VLK-II-2A|Valkyrie II VLK-II-2A]]</br>"}
-]
-vehicles = [
-    {"item": "[[Paladin#Paladin Prototype Defense System|Paladin Prototype Defense System]]</br>"},
-]
-mechparts = [
-    {"item": "[[JagerMech II#JM7-D|JagerMech II JM7-D]]</br>"},
-    {"item": "[[Valkyrie II#VLK-II-2A|Valkyrie II VLK-II-2A]]</br>"}
-]
-battlearmors = [
-    {"item": "[[Grenadier Battle Armor#|Grenadier Battle Armor]]</br>"},
-    {"item": "[[Infiltrator Mk II Battle Armor#|Infiltrator Mk II Battle Armor]]</br>"}
-]
-contracts = [
-    {"item": "[[Contracts#Contract for Mackie II Airdrop|Contract for Mackie II Airdrop]]</br>"},
-    {"item": "[[Contracts#Contract for Templar Airdrop|Contract for Templar Airdrop]]</br>"},
-    {"item": "[[Contracts#Permanent Contract for Grenadier BA Airdrop|Permanent Contract for Grenadier BA Airdrop]]</br>"},
-    {"item": "[[Contracts#Permanent Contract for Infiltrator Mk II BA Airdrop|Permanent Contract for Infiltrator Mk II BA Airdrop]]</br>"}
-]
 environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("factionStore.tpl")
+template = environment.get_template("pretty.tpl")
 
-results_filename = "factionTable.wiki"
-results_template = environment.get_template("factionStore.tpl")
-context = {
+example_dict =  {'Rasalhague.json': ['Weapon_Autocannon_LB10XAC',
+                     'Weapon_PPC_PPC_SNUBNOSE',
+                     'Weapon_Autocannon_UAC5_0-STOCK',
+                     'Weapon_SRM_SSRM2_0-STOCK',
+                     'Ammo_AmmunitionBox_LBX_CLUSTER_AC10',
+                     'Ammo_AmmunitionBox_LBX_SLUG_AC10',
+                     'Ammo_AmmunitionBox_Ultra_AC5',
+                     'Ammo_AmmunitionBox_Streak_SRM',
+                     'mechdef_viking_VKG-2F',
+                     'mechdef_vonrohrs_VON-4RH-5',
+                     'mechdef_vonrohrs_VON-4RH-6',
+                     'vehicledef_REDKITE',
+                     'vehicledef_UNNSVIN',
+                     'vehicledef_UNNSVIN_BALLISTIC',
+                     'vehicledef_UNNSVIN_MISSILE',
+                     'vehicledef_TAGDRONE',
+                     'vehicledef_VTOLDRONE',
+                     'mechdef_ba_krupp',
+                     'Weapon_Medium_Recoilless_BA',
+                     'Gear_Attachment_AutocannonFCS',
+                     'Gear_Attachment_PPCFCS',
+                     'Gear_Contract_Mech_Gunslinger_FRR',
+                     'Gear_Contract_BattleArmor_Krupp_Rasalhague']}
+
+codebase_dir = "/home/eadderley/fungit/BattleTech-Advanced"
+
+def render_factionstore(faction, items):
+    faction_name = faction[:-5]
+    results_filename = faction_name+"_store_Table.wiki"
+    faction_info=get_faction_specific_info(faction_name)
+    weapons = []
+    ammunitions = []
+    gears = []
+    mechs = []
+    mechparts = []
+    vehicles = []
+    battlearmors = []
+    contracts = []
+    for item in items:
+        if item.startswith("Weapon_"):
+            weapons.append(item)
+        elif item.startswith("Ammo_"):
+            ammunitions.append(item)
+        elif item.startswith("Gear_", "emod_")and not item.startswith("Gear_Contract_"):
+            gears.append(item)
+        elif item.startswith("mechdef_") and not item.startswith("mechdef_ba"):
+            mechs.append(item)
+        elif item.startswith("mechpartdef_"):
+            mechparts.append(item)
+        elif item.startswith("vehicledef_"):
+            vehicles.append(item)
+        elif item.startswith("mechdef_ba"):
+            battlearmors.append(item)
+        elif item.startswith("Gear_Contract_"):
+            contracts.append(item)
+    
+    for index, item in enumerate(weapons):
+        weapons[index] = get_display_name(item)
+    for index, item in enumerate(ammunitions):
+        ammunitions[index] = get_display_name(item)
+    for index, item in enumerate(gears):
+        gears[index] = get_display_name(item)
+    for index, item in enumerate(mechs):
+        mechs[index] = get_display_name(item)
+    for index, item in enumerate(mechparts):
+        mechparts[index] = get_display_name(item)
+    for index, item in enumerate(vehicles):
+        vehicles[index] = get_display_name(item)
+    for index, item in enumerate(battlearmors):
+        battlearmors[index] = get_display_name(item)
+    for index, item in enumerate(contracts):
+        contracts[index] = get_display_name(item)
+
+    context = {
+    "faction_info": faction_info,
     "weapons": weapons,
     "ammunitions": ammunitions,
     "gears": gears,
@@ -98,7 +87,31 @@ context = {
     "vehicles": vehicles, 
     "battlearmors": battlearmors,
     "contracts": contracts,
-}
-with open(results_filename, mode="w", encoding="utf-8") as results:
-    results.write(results_template.render(context))
-    print(f"... wrote {results_filename}")
+    }
+
+    with open(results_filename, mode="w", encoding="utf-8") as results:
+        results.write(results_template.render(context))
+        print(f"... wrote {results_filename}")
+
+
+def get_faction_specific_info(faction):
+    faction_lookup = {"Rasalhague": {"logo":"Cameron_logo.png", "name":"Terran Hegemony (Cameron)", "link":"Terran Hegemony"}},
+    return faction_lo          okup[faction]
+
+def get_display_name(item):
+    for root, dirs, files in os.walk(codebase_dir):
+        for file in files:
+            # Check if the file is the target JSON file
+            if file == item+".json":
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as json_file:
+                    data = json.load(json_file)
+                    # Check if 'Description' and 'UIName' exist
+                    ui_name = data['Description']['UIName']
+                    return ui_name
+
+
+if __name__ == "__main__":
+    for faction,items in example_dict.items():
+        render_factionstore(faction, items)
+        
--- templates/pretty.tpl
+++ templates/pretty.tpl
@@ -9,6 +9,29 @@
 ! Battle Armor
 ! Contracts
 |-
-{{ everything }}
-|-
-|}
+! [[File:{faction_info.logo}}|link={{faction_info.link}}|75px]] 
+[[{{faction_info.link|{{faction_info.name}}]]
+| {% for weapon in weapons %}
+[[ {{weapon}} ]]</br>
+{% endfor %}
+| {% for ammunition in ammunitions %}
+[[ {{ ammunition }}]]
+{% endfor %}
+| {% for gear in gears %}
+[[{{ gear }}]]
+{% endfor %}
+| {% for mech in mechs %}
+[[{{ mech }}]]
+{% endfor %}
+| {% for mechpart in mechparts %}
+[[{{ mechpart }}]]
+{% endfor %}
+| {% for vehicle in vehicles %}
+[[{{ vehicle }}]]
+{% endfor %}
+| {% for battlearmor in battlearmors %}
+[[{{ battlearmor }}]]
+{% endfor %}
+| {% for contract in contracts %}
+[[{{ contract }}]]
+{% endfor %}
